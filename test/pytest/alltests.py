ae66e8ba9ea87deda81253889f262761170d4d3d
Ben Wei <ben@staros.mobi>
2012-07-02 04:08:10 +0800

    add atoi python unittest

---
+++ test/pytest/alltests.py
@@ -0,0 +1,46 @@
+import unittest
+from ctypes import *
+
+bc = CDLL("./blibc.so")
+
+verbose = 1
+
+def boslog(msg):
+    global verbose
+    if verbose:
+        print msg
+
+
+class BOSTest(unittest.TestCase):
+    def runTest(self):
+        self.test_atoi();
+
+    def test_atoi(self):
+        '''
+        const char *itohex(uint32_t c, char *s, int size, int upper)
+        '''
+        c = 427
+        s = (c_byte*9)();
+        b = bc.itohex(c, s, c_int(9), c_int(0))
+        for i in range(0, 8):
+            boslog("[%d]%c" % (i, s[i]))
+        assert chr(s[7]) == 'b', 'incorrent data'
+
+def suite_blibc():
+    '''
+    suite = unittest.makeSuite(bosTestCase,'test')
+    '''
+    bosTestSuite = unittest.TestSuite()
+    testCase = BOSTest()
+    bosTestSuite.addTest(testCase)
+    return bosTestSuite
+
+
+def main():
+    suite1 = suite_blibc()
+    alltests = unittest.TestSuite((suite1))
+    runner = unittest.TextTestRunner()
+    runner.run(alltests);
+
+if __name__ == "__main__":
+    main()
