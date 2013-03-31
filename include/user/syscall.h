f4352e163dc78d36a1900cc3000591d1a69b7dfd
ben wei <ben@staros.mobi>
2013-03-31 11:21:15 +0800

    [syscall] move uput to syscall.h

--- include/user/syscall.h
+++ include/user/syscall.h
@@ -3,6 +3,9 @@
 
 #define BSYS_PUTS 8
 #define TRAP_SYSCALL 68
+
 unsigned int syscall(int num, unsigned int a1, unsigned int a2);
+
+void uputs(const char *msg);
 
 #endif
--- user/hello.c
+++ user/hello.c
@@ -1,7 +1,6 @@
 #include "stdio.h"
 #include "user/syscall.h"
 
-void uputs(const char *msg);
 
 int hello_main(void)
 {
